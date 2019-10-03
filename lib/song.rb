a4e5de5d71662c945008bba306b255b08d61c57a
Lauren Dove <elledoveinc@gmail.com>
2019-10-03 01:17:56 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  def self.count
+    @@count
+  end
+  
+  @@genres =[]
+  def self.genres
+  @@genres
+  end
+  
+  @@artists = []
+  def self.artists
+   @@artists
+  end
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+end
